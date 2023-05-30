ccc6403a816cb6917160fd823230091f3ba5b3b6
arnold <musaliokello@gmail.com>
2023-05-30 12:06:56 +0300

    Completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [x for x in num_list if x % 2 ==0]
+print (return_evens([0,1,2,3,4,5,6,7,8,9,10]))
+    
 
 def make_exclamation(sentence_list):
-    pass
+    return [s + "!" for s in sentence_list]
+    
